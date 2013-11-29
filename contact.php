70844a8932bdd1d3ed12d9491fb9000cca6907fc
piyaws <piyaws@rpi.edu>
2013-11-29 17:39:27 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,7 +1,40 @@
 <?php
 
+session_start();
 
+if (isset($_POST['send']) && $_POST['send'] == 'Send') {
+  if ((!isset($_POST['cf_name'])) || (!isset($_POST['cf_email'])) || (!isset($_POST['cf_message']))) {
+    $msg = "Please fill in all form fields.";
+  } else {
+    $field_name = $_POST['cf_name'];
+    $field_email = $_POST['cf_email'];
+    $field_message = $_POST['cf_message'];
 
+    $mail_to = 'contact@testmail.com';
+    $subject = 'Message from a site visitor '.$field_name;
+
+    $body_message = 'From: '.$field_name."\n";
+    $body_message .= 'E-mail: '.$field_email."\n";
+    $body_message .= 'Message: '.$field_message;
+
+    $headers = 'From: '.$field_email."\r\n";
+    $headers .= 'Reply-To: '.$field_email."\r\n";
+
+    $mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+    if ($mail_status) {
+      $msg = 'Thank you for the message. We will contact you shortly.';
+    }
+    else {
+      $msg = 'Message failed. Please, send an email to help@testmail.com';
+    }
+  }
+}
+
+if (isset($_POST['clear']) && $_POST['clear']=='Clear') {
+    header('Location: contact.php');
+    exit();
+  }
 
 ?>
 
@@ -28,12 +61,14 @@
         
         <div id="main">
           <h1>Contact</h1>
+          <?php if (isset($msg)) : echo $msg.'<br/>';
+                endif; ?>
             <form action="contact.php" method="post">
               <label for="name">Your name: </label><input type="text" name="cf_name" /><br>
               <label for="email">Your e-mail: </label><input type="text" name="cf_email" /><br>
               <label for="message">Message: </label><textarea name="cf_message" rows="5" cols="25" /></textarea><br/>
-              <input type="submit" value="Send">
-              <input type="submit" value="Clear">
+              <input type="submit" name="send" value="Send">
+              <input type="submit" name="clear" value="Clear">
             </form>
         </div>
 
