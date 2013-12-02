8ac0d15f434a0cfd88ab86d4fa75efdd803ace53
piyaws <piyaws@rpi.edu>
2013-12-01 22:26:41 -0500

    Update about.php

--- about.php
+++ about.php
@@ -23,9 +23,17 @@
         </a>
       </div>
       <div id="main">
-        <h1>About</h1>
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum justo eu erat dapibus, sit amet sollicitudin eros viverra. Pellentesque adipiscing a lacus vitae interdum. Nam pretium quis lacus elementum rhoncus. In augue mi, dapibus id lobortis eget, pharetra id neque. Suspendisse potenti. Integer metus diam, faucibus et adipiscing a, dignissim sed dui. Mauris tempor adipiscing libero, in suscipit sapien sagittis ut.
-        Morbi viverra tortor justo, quis suscipit risus ultricies cursus. Morbi sit amet blandit elit, eget interdum dolor. Maecenas hendrerit tincidunt ligula id porta. Vivamus felis sem, pellentesque sed congue ac, consequat a dolor. Maecenas hendrerit odio et elit luctus convallis. Nullam urna lorem, lacinia in ligula et, malesuada fermentum mi. Phasellus mollis consequat augue, vel viverra diam posuere ultricies. Morbi molestie facilisis orci, vel accumsan turpis feugiat nec. Aliquam tempus adipiscing orci eu venenatis. Mauris sollicitudin risus at fermentum consectetur. Cras eu convallis odio. Curabitur commodo tristique lectus eu pharetra.</p>
+        <div id="about">
+          <h1>About</h1>
+          <p>EntertainMe helps you keep track of all the things you simply must enjoy. Whether a book, movie, or band EntertainMe lets you track and organize your media.</p>
+        </div>
+        <div id="howitworks">
+          <h1>How It Works</h1>
+          <p>EntertainMe organizes your media into three categories, 'Now', 'Later', and 'Done.'</p>
+          <p>Whether a sweet new band or an awesome new video game, friends and acquaintances are always making great suggestions of things you must see or do right away. Simply add the media to your 'Now' list as a reminder to go check it out soon.</p>
+          <p>Life can be hectic. You don't always have time to read this month's bestseller or watch the hottest new film, but you don't want to forget about it. Simply add it to your 'Later' list to enjoy when life gives you a break. Drinks with tiny umbrellas optional.</p>
+          <p>You know that feeling. The one where someone is talking about a book they just finished, and it sounds super familiar. Did you read it? Or did you just hear someone else talk about it? Keep track of it all, simply. Once you've finished that enthralling book or epic movie, simply move it to your 'Done' list where it will serve as a reminder so you never forget!</p>
+        </div>
       </div>
       <div id="footer">
         <li><a href="about.php">About</a></li>
