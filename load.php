ce4bdec0524af23bd8eabb948f9a35301d41ca07
HollyLoomer <hollyloomer@gmail.com>
2013-12-01 19:37:31 -0500

    Update load.php

--- load.php
+++ load.php
@@ -25,12 +25,29 @@
         echo '<ul>';
         foreach ($call as $row) {
         	echo '<li>';
-          /*echo '<input type="text" name="title" value="'.$row['title'].'" readonly>';*/
-        	printf("Title: %s <span class='icons'>
+          echo '<form method="post" action="move.php">';
+          echo '<label for="title">'.$row['title'].'</label>';
+          echo '<input type="text" name="title" class="nodisplay" value="'.$row['title'].'" readonly>';
+        	if ($timePeriod == 'now') {
+            echo '<input type="submit" name="nowtolater" value="nowtolater" class="icon" />
+                  <input type="submit" name="nowtodone" value="nowtodone" class="icon" />
+                  <input type="submit" name="remove" value="remove" class="icon" />';
+        }
+          if ($timePeriod == 'later') {
+            echo '<input type="submit" name="latertonow" value="latertonow" class="icon" />
+                  <input type="submit" name="latertodone" value="latertodone" class="icon" />
+                  <input type="submit" name="remove" value="remove" class="icon" />';
+          }
+          if ($timePeriod == 'done') {
+            echo '<input type="submit" name="donetonow" value="donetonow" class="icon" />
+                  <input type="submit" name="donetolater" value="donetolater" class="icon" />';
+          }
+          echo '</form>';
+          /*printf("Title: %s <span class='icons'>
             <span class='icon1'>&#x2611</span>
             <span class='icon2'>&#x2799</span>
             <span class='icon3'>&#x2612</span></span>
-        		<br/>", $row['title']);
+        		<br/>", $row['title']);*/
         	printf("Description: %s ", $row['description']);
         	echo '</li>';
         }
