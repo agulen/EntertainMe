6e86107423b151fd903ba2fa8446acfaf4443e95
piyaws <piyaws@rpi.edu>
2013-12-01 19:15:04 -0500

    Update later.php

--- later.php
+++ later.php
@@ -51,7 +51,7 @@
 
 		<div id="all">
   		<div class="logo">
-  			Logo area.
+  			<img src="http://i.minus.com/iI7WTHovllxD1.png" alt="logo" width="400px" height="auto" >
   		</div>
 			<div id="main">
 				<div id="pageDescription">
