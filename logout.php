1ccafbab31ca812012dc510e5b2225810e2f82b9
HollyLoomer <hollyloomer@gmail.com>
2013-12-05 18:22:34 -0500

    Added a comment

--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
+//simple logout file. when this is linked to, the user session is destroyed and redirects to the homepage where they must sign up or log in 
 	session_start();
 	setcookie(session_name(),'',time() - 72000);
 	session_unset();
